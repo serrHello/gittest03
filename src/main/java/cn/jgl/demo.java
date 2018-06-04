ed7aee5200487b758d536ceea36ccf658ccff973
syshello <serrHello@163.com>
2018-06-04 08:47:23 +0800

    first commit

---
+++ src/main/java/cn/jgl/demo.java
@@ -0,0 +1,22 @@
+package cn.jgl;
+
+/**
+ * @author 子龙
+ * @version 1.0
+ * @title 标题
+ * @date 2018/6/3
+ * @package cn.jgl
+ * @company www.cn.jgl
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("常山赵子龙");
+       
+    }
+
+    private void test(){
+        System.out.println("serrHello");
+    }
+
+}
